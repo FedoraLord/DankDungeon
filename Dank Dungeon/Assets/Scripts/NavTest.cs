83eebe7bbf14508551d4770cb60d3e2d0c7aaf7a
FedoraLord <jerkrouse@gmail.com>
2018-02-20 14:56:33 -0600

    Project files and basic folder structure

---
+++ Dank Dungeon/Assets/Scripts/NavTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavTest : MonoBehaviour {
+
+    public float agentClimb;
+    public float agentHeight;
+    public float agentRadius;
+    public float agentSlope;
+    public int agentTypeID;
+    public float minRegionArea;
+    public bool overrideTileSize;
+    public bool overrideVoxelSize;
+    public int tileSize;
+    public float voxelSize;
+
+    // Use this for initialization
+    void Start () {
+        //NavMeshBuildSettings buildSettings = new NavMeshBuildSettings();
+        //{
+        //    agentClimb = agentClimb,
+        //    agentHeight = agentHeight,
+        //    agentRadius = agentRadius,
+        //    agentSlope = agentSlope, 
+        //    agentTypeID = agentTypeID,
+        //    minRegionArea = minRegionArea,
+        //    overrideTileSize = overrideTileSize,
+        //    overrideVoxelSize = overrideVoxelSize,
+        //    tileSize = tileSize,
+        //    voxelSize = voxelSize
+        //};
+
+        //NavMeshBuildSource asdf = new NavMeshBuildSource();
+        //{
+        //    area = 0,
+        //    component = 0,
+        //    shape = 0,
+        //    size = 0,
+        //    sourceObject = 0,
+        //    transform = 0
+        //}
+        //var aaaa = NavMeshBuilder.BuildNavMeshData(buildSettings, new List<NavMeshBuildSource>() { asdf }, new Bounds(), new Vector3(), Quaternion.identity);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Dank Dungeon/Assets/Scripts/Something.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Something : MonoBehaviour {
+
+    public GameObject prefab;
+    public GameObject objectReference;
+
+    public void DoThing()
+    {
+        objectReference = Instantiate(prefab, transform.position, Quaternion.identity);
+    }
+}
