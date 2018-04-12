78405eb267a7a8cbb9142c2d21a7b9b7a8d6c65e
Javier Rodriguez <javier.rodval95@gmail.com>
2018-04-12 11:24:09 -0500

    Scripts for pause menu
    Scripts to interact with pause menus

---
+++ Dank Dungeon/Assets/Scripts/ItemDescription.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ItemDescription : MonoBehaviour {
+    public GameObject[] description = new GameObject[6];
+
+    public void ShortSwordDescription()
+    {
+        Rendering(0);
+    }
+
+    public void BroadSwordDescription()
+    {
+        Rendering(1);
+    }
+
+    public void KatanaDescription()
+    {
+        Rendering(2);
+    }
+
+    public void ArmorDescription()
+    {
+        Rendering(3);
+    }
+
+    public void LargeHealthPotionDescription()
+    {
+        Rendering(4);
+    }
+
+    public void LargeCurePotionDescription()
+    {
+        Rendering(5);
+    }
+
+    public void None()
+    {
+        Rendering(description.Length);
+    }
+
+    private void Rendering(int index)
+    {
+        for (int count = 0; count < description.Length; count++)
+            if (count != index)
+                description[count].SetActive(false);
+            else
+                description[count].SetActive(true);
+    }
+
+  }
---
+++ Dank Dungeon/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PauseMenu : MonoBehaviour {
+
+    public string mainMenuScene = "StartMenu";
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject[] contentArray = new GameObject[7];
+    private float fIndex;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused) {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        fIndex = contentArray[1].transform.position.y;
+        AllButton();
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        
+    }
+
+    public void AllButton()
+    {
+        float y = fIndex; 
+        contentArray[0].transform.DetachChildren();
+
+        for (int count = 1; count < contentArray.Length; count++)
+        {
+            contentArray[count].transform.position = new Vector3(contentArray[count].transform.position.x, y, contentArray[count].transform.position.z);
+            y -= 31f;
+        }
+
+        for (int count = 1; count < contentArray.Length; count++)
+            contentArray[count].transform.SetParent(contentArray[0].transform);
+    }
+
+    public void OnlyWeapons()
+    {
+        float y = fIndex;
+        contentArray[0].transform.DetachChildren();
+
+        for (int count = 1; count < 4; count++)
+        {
+            contentArray[count].transform.position = new Vector3(contentArray[count].transform.position.x, y, contentArray[count].transform.position.z);
+            y -= 31f;
+        }
+
+        for (int count = 1; count < 4; count++)
+            contentArray[count].transform.SetParent(contentArray[0].transform);
+    }
+
+    public void OnlyOthers()
+    {
+        float y = fIndex;
+        contentArray[0].transform.DetachChildren();
+
+        for (int count = 4; count < contentArray.Length; count++)
+        {
+            contentArray[count].transform.position = new Vector3(contentArray[count].transform.position.x, y, contentArray[count].transform.position.z);
+            y -= 31f;
+        }
+
+        for (int count = 4; count < contentArray.Length; count++)
+            contentArray[count].transform.SetParent(contentArray[0].transform);
+    }
+
+    public void SettingsButton()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(mainMenuScene);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
